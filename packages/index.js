af61dc66b39b578138791270fb633eb37f194f6b
e <jiangqisy@chinamobile.com>
2021-01-29 14:58:45 +0800

    init pro

---
+++ packages/index.js
@@ -0,0 +1,17 @@
+import test from "./test";
+const components = [test];
+const install = function(Vue) {
+  if (install.installed) return;
+  components.map(component => {
+    Vue.use(component);
+  });
+};
+//  全局引用可自动安装
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+export default {
+  install,
+  test
+};
+export { test };
---
+++ packages/test/index.js
@@ -0,0 +1,4 @@
+import test from "./src/test";
+export default Vue => {
+  Vue.component(test.name, test);
+};
