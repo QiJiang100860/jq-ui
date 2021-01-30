90b6cac730882dec84963be988ce83ed8b604e11
e <jiangqisy@chinamobile.com>
2021-01-30 14:52:49 +0800

    delete:删除无用组件

--- examples/views/About.vue
+++ examples/views/About.vue
@@ -3,15 +3,5 @@
     <h1>This is an about page</h1>
     <sy-wrapper></sy-wrapper>
     <sy-nodata size="mini"></sy-nodata>
-    <sy-slider
-      :data="{
-        index: '1',
-        siteName: '站点名称法撒旦',
-        max: '1000',
-        val: '400',
-        unit: 'kW'
-      }"
-      theme="#000000"
-    ></sy-slider>
   </div>
 </template>
