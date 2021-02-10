c06a9f8f70aeb82eedd35e9a7243cf1b9d3a3fcb
e <jiangqisy@chinamobile.com>
2021-02-10 16:02:02 +0800

    ex-major:首页交互细节优化

--- examples/views/Home/index.vue
+++ examples/views/Home/index.vue
@@ -1,37 +1,73 @@
 <template>
   <div class="home">
-    <div class="logo">JQ</div>
+    <div class="logo" @click="toggle" :class="open ? 'active' : ''">JQ</div>
     <div style="margin-bottom: 10px">JQ祝您新年新气象，全家幸福，合家欢乐</div>
     <div class="line"></div>
     <div style="margin-top: 10px; margin-bottom: 40px">IT's NOT JQUERY</div>
 
     <div class="letter">
-      <div class="letter-card">
+      <div class="letter-card" :class="open ? 'active' : ''">
         <span class="title">牛</span>
         <div class="decp">
           【JQ】<span style="color: #ff0000">*vue开发深度总结</span
-          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span style="font-weight:700;font-size:12px;margin-left:10px;color:#ff0000" @click="goHome">click</span>
+          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span
+            style="
+              font-weight: 700;
+              font-size: 12px;
+              margin-left: 10px;
+              color: #ff0000;
+            "
+            @click="goHome"
+            >click</span
+          >
         </div>
       </div>
-      <div class="letter-card">
+      <div class="letter-card" :class="open ? 'active' : ''">
         <span class="title">年</span>
         <div class="decp">
           【jq-ui】<span style="color: #ff0000">*vue开发效率提升神器</span
-          >，让你事半功倍，让你欲罢不能<span style="font-weight:700;font-size:12px;margin-left:10px;color:#ff0000" @click="goTool">click</span>
+          >，让你事半功倍，让你欲罢不能<span
+            style="
+              font-weight: 700;
+              font-size: 12px;
+              margin-left: 10px;
+              color: #ff0000;
+            "
+            @click="goTool"
+            >click</span
+          >
         </div>
       </div>
-      <div class="letter-card">
+      <div class="letter-card" :class="open ? 'active' : ''">
         <span class="title">大</span>
         <div class="decp">
           【jq-ui】<span style="color: #ff0000">*通用解决方案</span
-          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span style="font-weight:700;font-size:12px;margin-left:10px;color:#ff0000" @click="goUtil">click</span>
+          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span
+            style="
+              font-weight: 700;
+              font-size: 12px;
+              margin-left: 10px;
+              color: #ff0000;
+            "
+            @click="goUtil"
+            >click</span
+          >
         </div>
       </div>
-      <div class="letter-card">
+      <div class="letter-card" :class="open ? 'active' : ''">
         <span class="title">吉</span>
         <div class="decp">
           【jq-ui】<span style="color: #ff0000">*新年快乐</span
-          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span style="font-weight:700;font-size:12px;margin-left:10px;color:#ff0000" @click="goAlert">click</span>
+          >，欢迎使用我，对我得多多得支持，我会努力得更新出好多东西哦<span
+            style="
+              font-weight: 700;
+              font-size: 12px;
+              margin-left: 10px;
+              color: #ff0000;
+            "
+            @click="goAlert"
+            >click</span
+          >
         </div>
       </div>
     </div>
@@ -40,24 +76,28 @@
 
 <script>
 export default {
-  mounted() {
-    this.init();
+  data() {
+    return {
+      open: false,
+    };
   },
   methods: {
-    init() {},
+    toggle() {
+      this.open = !this.open;
+    },
     goHome() {
       this.$router.push("/frame/jqwrapper");
     },
     goTool() {
       this.$router.push("/frame/jqmap");
     },
-    goUtil(){
+    goUtil() {
       this.$router.push("/frame/jqJsonp");
     },
-    goAlert(){
-      alert("最后再啰嗦下，祝您新年快乐，红包多多")
+    goAlert() {
+      alert("最后再啰嗦下，祝您新年快乐，红包多多");
       this.$router.push("/frame/jqecharts");
-    }
+    },
   },
 };
 </script>
@@ -82,6 +122,18 @@ export default {
     font-weight: 700;
     cursor: pointer;
     margin-bottom: 40px;
+    transition: all 0.3s;
+    &.active {
+      width: 80px;
+      height: 80px;
+      line-height: 80px;
+      background: #000;
+      color: #fff;
+    }
+    &:hover {
+      background: #333;
+      color: #aaa;
+    }
   }
   .line {
     height: 1px;
@@ -99,12 +151,12 @@ export default {
       width: 150px;
       height: 200px;
       margin: 0 10px;
-
       border-radius: 8px;
       background: #000;
       border: solid 1px transparent;
       cursor: pointer;
       transition: all 0.3s;
+      transition-delay: 0.1s;
       position: relative;
       .title {
         position: absolute;
@@ -113,6 +165,8 @@ export default {
         margin-left: -20px;
         margin-top: -30px;
         transition: all 0.3s;
+        transition-delay: 0.1s;
+
         font-size: 40px;
         color: #fff;
       }
@@ -121,11 +175,30 @@ export default {
         color: #fff;
         opacity: 0;
         transition: all 0.6s;
+        transition-delay: 0.1s;
+
         padding: 10px;
         padding-top: 40px;
         font-size: 14px;
         line-height: 28px;
       }
+      &.active {
+        background: #fff;
+        border-color: #dedede;
+        .title {
+          top: 5%;
+          margin-top: 0px;
+          margin-left: -10px;
+          color: #333;
+          font-size: 20px;
+          font-weight: 700;
+        }
+        .decp {
+          opacity: 1;
+          color: #333;
+        }
+      }
+
       &:hover {
         background: #fff;
         border-color: #dedede;
