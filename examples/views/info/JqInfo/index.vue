495618ceadae4f1697258a39a3cb2b2e8e2a1d90
e <jiangqisy@chinamobile.com>
2021-02-23 13:51:04 +0800

    feat: highlight test

--- examples/views/info/JqInfo/index.vue
+++ examples/views/info/JqInfo/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>项目介绍</div>
+  <div>
+    <pre>
+      <code id="code" class="language-html">
+        <template></template>
+        <div>
+          <ul class="aaaaa">
+            <li>1</li>
+            <li>2</li>
+          </ul>
+        </div>
+      </code>
+    </pre>
+  </div>
 </template>
+
+<script>
+import hljs from "highlight.js";
+import "highlight.js/styles/vs.css";
+export default {
+  mounted() {
+    this.testCode();
+  },
+  methods: {
+    testCode() {
+      hljs.initHighlightingOnLoad();
+      let el = document.getElementById("code");
+      let stext = el.innerHTML;
+      let elStr = this.html2Escape(stext);
+      el.innerHTML = elStr;
+      hljs.highlightBlock(el);
+    },
+    html2Escape(sHtml) {
+      return sHtml.replace(/[<>&"]/g, function(c) {
+        return { "<": "&lt;", ">": "&gt;", "&": "&amp;", '"': "&quot;" }[c];
+      });
+    }
+  }
+};
+</script>
